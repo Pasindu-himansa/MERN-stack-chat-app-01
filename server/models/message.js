2e0373c9988e6b0415b4072af4de71949d44386c
Pasindu Subasinghe <phimansa@gmail.com>
2026-01-18 20:00:36 +0530

    message router setup

---
+++ server/models/message.js
@@ -0,0 +1,13 @@
+import mongoosefrom "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId : {type: mongoose,Schema, Types.ObjectId, ref:"User", required : true }
+    recieverId : {type: mongoose,Schema, Types.ObjectId, ref:"User", required : true }
+    text : {type : string,}, 
+    image: {type : string,},
+    seen: {type: Boolean, default:false }
+}, { timestamps: true });
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./library/database.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 // Create Express app and http Server
 const app = express();
@@ -17,6 +18,7 @@ app.use(cors());
 //Routes setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 // Connect to MongoDB
 await connectDB();
